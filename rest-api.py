b6bd9bd9b222f74c41af5c49a5609a4220b3d7af
MohammadMaasir <maasir554@gmail.com>
2025-03-23 11:21:32 +0530

    more api endpoints and local pdf precessing feature added

--- rest-api.py
+++ rest-api.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI, Request, HTTPException
 import json
-from solution import pushTablesInSequence, pdfFromLink
+from solution import pushTablesInSequence, pdfFromLink, localPush
+
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def read_root():
+    return {"message": "Go to /static to see the files"}
+
+
+
 # POST endpoint to handle raw JSON
 @app.post("/extract-and-push")
+
 async def process_json(request: Request):
     # Read the raw JSON body
     raw_body = await request.body()
@@ -37,6 +49,50 @@ async def process_json(request: Request):
             data['endBefore'] = -1
 
         update = await pushTablesInSequence(pdf_bin, int(data['startFrom']), int(data['endBefore']), data['pdfLink'])
+
+        return {
+            "message": "All pages pushed successfully to database.",
+            "data": data,
+            "update": update
+        }
+    
+    except Exception as err:
+        raise HTTPException(status_code=500, detail=str(err))
+
+#for local pdf
+
+@app.post("/extract-local")
+async def process_json(request: Request):
+    # Read the raw JSON body
+    raw_body = await request.body()
+
+    data = json.loads(raw_body)
+    
+    """
+        format of body: {
+            pdfName: string,
+            startFrom: number,
+            endBefore: number,
+        }
+    """
+    
+    print(data)
+    
+    try:
+        
+        if 'pdfName' not in data:
+          Exception("please provide ")  
+
+        # pdf_bin = pdfFromLink(data['pdfLink']);
+        
+
+        if 'startFrom' not in data:
+            data['startFrom'] = 0
+        
+        if 'endBefore' not in data:
+            data['endBefore'] = -1
+
+        update = await localPush(data['pdfName'], int(data['startFrom']), int(data['endBefore']), "doc1")
 
         return {
             "message": "All pages pushed successfully to database.",
--- solution.py
+++ solution.py
@@ -7,8 +7,8 @@ from prisma import Prisma
 import pytesseract
 from PIL import Image
 import fitz
+import pymupdf
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
 
 
 # client = genai.Client(api_key="AIzaSyC_eBiiJRNneilrj30VnwsK2JkhoIG6Jlk")
@@ -122,20 +122,143 @@ links = [
 #     db = Prisma()
 
 
-async def main() -> None:
+# async def main() -> None:
+#     db = Prisma()
+#     print("session started")
+#     await db.connect()
+#     myPdf = pdfFromLink(links[1])
+    
+#     to_push = pushTablesInSequence(myPdf, 29,30, links[1])
+    
+#     pushed = await db.tablerecord.create(data = to_push)
+    
+
+#     print(f'\n\nPushed data:\n{pushed}\n\n')
+    
+#     await db.disconnect()
+
+# if __name__ == '__main__':
+#     asyncio.run(main())
+
+async def localPush(pdfName, start, end, origin):
     db = Prisma()
+    
     print("session started")
+    
     await db.connect()
-    myPdf = pdfFromLink(links[1])
-    
-    to_push = pushTablesInSequence(myPdf, 29,30, links[1])
-    
-    pushed = await db.tablerecord.create(data = to_push)
-    
 
-    print(f'\n\nPushed data:\n{pushed}\n\n')
+    reader = PdfReader(pdfName)
     
+    size = len(reader.pages)
+    
+    if(end==-1):
+        end = size
+
+    for i in range(start, end):
+        page = reader.pages[i]
+
+        pageText = page.extract_text()
+
+        if(pageText == ""):
+            pdf = pymupdf.open(pdfName)
+            pdf_document_images = fitz.open(stream= pdf.getvalue(), filetype="pdf")
+            page_image = pdf_document_images[i]
+            zoom = 600 / 72
+            matrix = fitz.Matrix(zoom, zoom)
+            pix = page_image.get_pixmap(matrix=matrix)
+            # Convert to PIL Image
+            pil_img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+ 
+            pageText = pytesseract.image_to_string(pil_img)
+        
+        tableData =  getGeminiResponse(f"""
+                                       the following text is extracted from a pdf. it may contain paragraphs and tables.
+                                       you have to read that text and convert it to a usable format.
+                                       I want you to convert it to a JSON
+
+                                       please ensure that your output is a valid JSON. and represents the original table as close as possible.
+                                       response only in JSON format, and no extra text/words shall be in your response.
+
+                                       the output may be a nested json, and its keys should map to the heder of the table. please do not separate the header and data.
+                                       please make sure you do not hallucinate write any number data with precision and accuracy as it represents sensitive statistical data.
+                                       
+                                       Raw Text is as follows:
+                                       {pageText}
+                                        """)
+        title = getGeminiResponse(f"Please output a suitable title for the provided raw text. do not write anything other than the title in your response. raw Text: {pageText}")
+        
+        description = getGeminiResponse(f"Please output a suitable brief description for the provided raw text. do not write anything other than the description in your response. raw Text: {pageText}")
+
+        keywords = getGeminiResponse(f"Please output a list of 5 keys corresponding to the provided raw text. do not write anything other than the keys list in your response. raw Text: {pageText}")
+
+        data_to_push = {
+            "title": title,
+            "description": description,
+            "keywords": keywords,
+            "table_json": tableData, 
+            "docOfOrigin": origin
+        }
+    
+        pushed = await db.tablerecord.create(data = data_to_push)
+    
+        print(f'\n\nPushed data:\n{pushed}\n\n')
+
+        
     await db.disconnect()
+    return "Done Successfully"
 
-if __name__ == '__main__':
+
+
+async def PhotoPush(imgName):
+    
+    db = Prisma()
+    
+    print("session started")
+    
+    await db.connect()
+       
+    page_image = Image.open(imgName)
+    
+    pageText = pytesseract.image_to_string(page_image, config="--psm 6")
+     
+    tableData =  getGeminiResponse(f"""
+                                   the following text is extracted from a pdf. it may contain paragraphs and tables.
+                                   you have to read that text and convert it to a usable format.
+                                   I want you to convert it to a JSON
+
+                                   please ensure that your output is a valid JSON. and represents the original table as close as possible.
+                                   response only in JSON format, and no extra text/words shall be in your response.
+
+                                   the output may be a nested json, and its keys should map to the heder of the table. please do not separate the header and data.
+                                   please make sure you do not hallucinate write any number data with precision and accuracy as it represents sensitive statistical data.
+                                   
+                                   Raw Text is as follows:
+                                   {pageText}
+                                    """)
+    title = getGeminiResponse(f"Please output a suitable title for the provided raw text. do not write anything other than the title in your response. raw Text: {pageText}")
+     
+    description = getGeminiResponse(f"Please output a suitable brief description for the provided raw text. do not write anything other than the description in your response. raw Text: {pageText}")
+
+    keywords = getGeminiResponse(f"Please output a list of 5 keys corresponding to the provided raw text. do not write anything other than the keys list in your response. raw Text: {pageText}")
+
+    data_to_push = {
+        "title": title,
+        "description": description,
+        "keywords": keywords,
+        "table_json": tableData, 
+        "docOfOrigin": 'image'
+    }
+    
+    pushed = await db.tablerecord.create(data = data_to_push)
+    
+    print(f'\n\nPushed data:\n{pushed}\n\n')
+
+        
+    await db.disconnect()
+    return "Done Successfully"
+
+async def main():
+    await PhotoPush("photo4.png")
+
+if __name__ == "__main__":
     asyncio.run(main())
